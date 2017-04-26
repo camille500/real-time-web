8ca7040c05c337e843525aa8b9a2dfe70cecca1c
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-04-26 15:41:12 +0200

    Twitter Login for Heroku domain

--- routes/oAuth.js
+++ routes/oAuth.js
@@ -21,7 +21,7 @@ const consumer = new oauth.OAuth(
   consumerKey,
   consumerSecret,
   '1.0A',
-  'http://127.0.0.1:3000/twitter/callback',
+  'https://real-time-web-cmd.herokuapp.com/twitter/callback',
   'HMAC-SHA1'
 );
 
